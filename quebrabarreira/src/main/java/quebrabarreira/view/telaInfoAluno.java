4070bfa6fd4d0ccea7499a00fd3f79ad2078e5c5
EDUARDO CAMARGO NEVES <ecn19@inf.ufpr.br>
2022-09-15 18:28:24 -0300

    telas de inicio e solicitacao

--- quebrabarreira/src/main/java/quebrabarreira/view/telaInfoAluno.java
+++ quebrabarreira/src/main/java/quebrabarreira/view/telaInfoAluno.java
@@ -1,11 +1,15 @@
 package quebrabarreira.view;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import java.awt.Color;
+import java.awt.BorderLayout;
 
 public class telaInfoAluno extends JFrame {
+
+    private JLabel Titulo1,Titulo2,Titulo3,Titulo4;
 
     public telaInfoAluno() {
 
@@ -19,27 +23,49 @@ public class telaInfoAluno extends JFrame {
         //Binicio = new JButton("voltar inicio");
         //Binicio.addActionListener(this);
 
-        JPanel panel1JPanel =  new JPanel();
-        panel1JPanel.setBounds(0,0,250,500);
-        panel1JPanel.setBackground(Color.BLUE);
+        Titulo1 = new JLabel();
+        Titulo1.setText("Materia já feitas");
+        Titulo1.setBounds(65, 10, 120, 20);
 
-        JPanel panel2JPanel =  new JPanel();
-        panel2JPanel.setBounds(250,0,250,500);
-        panel2JPanel.setBackground(Color.RED);
+        Titulo2 = new JLabel();
+        Titulo2.setText("Materia que falta da barreira");
+        Titulo2.setBounds(20, 10, 210, 20);
 
-        JPanel panel3JPanel =  new JPanel();
-        panel3JPanel.setBounds(500,0,250,500);
-        panel3JPanel.setBackground(Color.GREEN);
+        Titulo3 = new JLabel();
+        Titulo3.setText("Materia ofertadas esse semestre");
+        Titulo3.setBounds(5, 10, 240, 20);
 
-        //JPanel panelBotoes =  new JPanel();
-        //panelBotoes.setBounds(150, 200, 200, 100);
-        //panelBotoes.add(Binicio);
+        Titulo4 = new JLabel();
+        Titulo4.setText("Dados do ultimo semestre");
+        Titulo4.setBounds(290, 100, 240, 50);
+
+
+
+        JPanel panelMateriasCursadas =  new JPanel(null);
+        panelMateriasCursadas.setBounds(0,0,250,300);
+        panelMateriasCursadas.add(Titulo1);
+        panelMateriasCursadas.setBackground(Color.BLUE);
+
+        JPanel panelMateriasFalta =  new JPanel(null);
+        panelMateriasFalta.setBounds(250,0,250,300);
+        panelMateriasFalta.add(Titulo2);
+        panelMateriasFalta.setBackground(Color.RED);
+
+        JPanel panelMateriasOfertadas =  new JPanel(null);
+        panelMateriasOfertadas.setBounds(500,0,250,300);
+        panelMateriasOfertadas.add(Titulo3);
+        panelMateriasOfertadas.setBackground(Color.GREEN);
+
+        JPanel paneldados =  new JPanel(null);
+        paneldados.setBounds(0,200,750,300);
+        paneldados.add(Titulo4);
+        paneldados.setBackground(Color.YELLOW);
 
         setVisible(true);
-        this.add(panel1JPanel);
-        this.add(panel2JPanel);
-        this.add(panel3JPanel);
-
+        this.add(panelMateriasCursadas);
+        this.add(panelMateriasFalta);
+        this.add(panelMateriasOfertadas);
+        this.add(paneldados);
     }
     
 }
