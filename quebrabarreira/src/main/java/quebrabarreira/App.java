098cf1f1348e585fff6d72de353079675a2e437c
atc19 <atc19@inf.ufpr.br>
2022-09-16 16:25:32 -0300

    teste mto loco mostra ae pro prof :)))

--- quebrabarreira/src/main/java/quebrabarreira/App.java
+++ quebrabarreira/src/main/java/quebrabarreira/App.java
@@ -1,10 +1,12 @@
 package quebrabarreira;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 import quebrabarreira.controllers.AlunoController;
-import quebrabarreira.controllers.FormularioController;
-import quebrabarreira.view.InterfaceGrafica;
+import quebrabarreira.controllers.PedidoController;
 
 public class App 
 {
@@ -12,30 +14,27 @@ public class App
     {
         int ano = 2019; 
         String historicoPath = "./src/main/resources/historicoAluno.csv";    
-        String gradePath = "./src/main/resources/grade2019.csv";
+        String gradePath = "./src/main/resources/grades2011_2019.csv";
         
         AlunoController aluno = new AlunoController(ano, historicoPath, gradePath);
-        FormularioController formController = new FormularioController();
-
-        // TelaInicial tela = new TelaInicial(aluno);
-        // tela.Tela_Inicial(aluno);
-
-        // tela info aluno
-        // System.out.println(aluno.getAluno().getGRR());
-        // System.out.println(aluno.getAluno().getNome());
-        // System.out.println(aluno.getAluno().getIra());
-        // System.out.println(aluno.getAluno().getTaxaAprovacaoUltimoPeriodo());
-        // System.out.println(aluno.getAluno().getUltimoAnoCursado());
-        // System.out.println(aluno.getAluno().getUltimoPeriodoCursado());
         
-        // PedidoController Pedido = new PedidoController();
+        aluno.getAluno().setIra(0.6);
+        aluno.getAluno().setTaxaAprovacaoUltimoPeriodo(0.7);
 
-        // FileHandle file = new FileHandle();
-        // List<HashMap<String, String>> result;
-        // result = file.getCsv(gradePath);
-
-
-        InterfaceGrafica.Invoke(aluno, formController);
-
+        List<String> materias = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String nextLine = scanner.nextLine();
+            if ( nextLine.equals("") ) {
+               break;
+            }
+            materias.add(nextLine);
+        }
+        scanner.close();
+        
+        PedidoController pedido = new PedidoController("GRR20190471", materias);
+        pedido.getPedido().savePedido();
+        System.out.println(pedido.getPedido().trataPedido(aluno));
+        
     }
 }
